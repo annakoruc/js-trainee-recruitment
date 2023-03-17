b42d53d2c540c11267f41e66a06b949c1bdd00a9
Ania <aniakoruc@gmail.com>
2023-03-17 17:53:27 +0100

    add hooks to change class when scroll into view

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import styles from "../styles/componentsStyle/AnimalCard.module.css";
+import { Heading } from "./Heading";
 
 type CardProps = {
   animal: string;
@@ -14,7 +15,7 @@ export const AnimalCard = ({ animal, text, src, id }: CardProps) => {
   return (
     <div className={styles.card} id={id}>
       <main>
-        <p>{animal.toUpperCase()}</p>
+        <Heading as="h2">{animal.toUpperCase()}</Heading>
         {text}
       </main>
       <Image src={src} width={817} height={431} alt={animal} />
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,40 +1,32 @@
-import React, { useRef, useState } from "react";
-import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import useHeadings from "@/hooks/useHeadings";
+import useScrollSpy from "@/hooks/useScrollSpy";
 
 import styles from "../styles/componentsStyle/Sidebar.module.css";
 
-const navList = [
-  { name: "Polar Bear", id: "#bear" },
-  { name: "cheetah", id: "#cheetah" },
-  { name: "panda", id: "#panda" },
-  { name: "fox", id: "#fox" },
-  { name: "squirrel", id: "#squirrel" },
-  { name: "butterfly", id: "#butterfly" },
-  { name: "elephant", id: "#elephant" },
-];
-
 export const SideBar = () => {
-  const [selectedLink, setSelectedLink] = useState("");
-  let router = useRouter();
+  const headings = useHeadings();
+  const activeId = useScrollSpy(
+    headings.map(({ id }) => id),
+    { rootMargin: "0% 0% -25% 0%" }
+  );
 
   return (
     <nav className={styles.sidebar}>
       <p>Your new gang</p>
       <ul>
-        {navList.map((val, key) => {
+        {headings.map((val) => {
           return (
             <li
-              className={`row ${selectedLink === val.id ? "active_link" : ""}`}
-              key={key}
+              key={val.id}
+              className={activeId === val.id ? "active_link" : ""}
               onClick={() => {
-                setSelectedLink(val.id);
-
                 document
-                  .querySelector(val.id)!
+                  .getElementById(val.id)!
                   .scrollIntoView({ behavior: "smooth" });
               }}
             >
-              {val.name}
+              {val.text}
             </li>
           );
         })}
